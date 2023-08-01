0ccebdf71feb7741e31c2406df142827394e0ccc
Piyushsofto <piyush.dutta@softobotics.com>
2023-08-01 13:07:24 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Navigation from './components/Navigation';
+import { Hero } from './components/Hero';
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <Navigation/>
+      <Hero/>
+    </div>
+  );
+}
+
+export default App;
